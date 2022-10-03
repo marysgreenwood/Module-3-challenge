93af486b385a8f375a461360e2daf5e868493de3
marysgreenwood <marysgreenwood@gmail.com>
2022-10-03 17:00:27 -0400

    added pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,163 @@
+//Stopped at line 133--must find out how to set up conditional statements for constructing array based on passwordOptions
+
 // Assignment code here
 
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function getPasswordOptions() {
+  // 3)
+  // Variable to store length of password from user input
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?'),
+    10
+  );
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (Number.isNaN(length)) {
+    alert('Password length must be provided as a number');
+    return null;
+  }
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  if (length < 8) {
+    alert('Password length must be at least 8 characters');
+    return null;
+  }
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+  if (length > 128) {
+    alert('Maximum password length 128 characters');
+    return null;
+  }
+  // Variable to store boolean regarding the inclusion of special characters
+  var hasSpecialCharacters = confirm(
+    'Click OK to confirm including special characters.'
+  );
+
+  // Variable to store boolean regarding the inclusion of numeric characters
+  var hasNumericCharacters = confirm(
+    'Click OK to confirm including numbers.'
+  );
+  // Variable to store boolean regarding the inclusion of lowercase characters
+  var hasLowercaseCharacters = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );
+  // Variable to store boolean regarding the inclusion of uppercase characters
+  var hasUppercaseCharacters = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );
+  // Conditional statement to check if user does not include any types of characters. Password generator ends if all four variables evaluate to false
+  // Object to store user input
+  var passwordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    //more options here
+  };
+
+  return passwordOptions;
+}
+
+
+
+//Declare generatePassword function
+var generatePassword = function(){
+  
+  
+
+  
+  // Function for getting a random element from an array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
 
 // Write password to the #password input
 function writePassword() {
